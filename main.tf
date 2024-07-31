cbab1d061e10290e733fb7666eb9a58179911f56
shresis <162896197+shresis@users.noreply.github.com>
2024-07-31 22:47:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "terraform-instance" {
     ami = "ami-0ad21ae1d0696ad58"
     instance_type = "t2.micro"
-    key_name = "terra"
+    key_name = "integration_jt"
     tags = {
       "Name" = "test"
     }
